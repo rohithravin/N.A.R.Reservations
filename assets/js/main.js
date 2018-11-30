a11f83f5c06f357ca60982499c6ae7bbbca08ef2
navyaannam <nannam@purdue.edu>
2018-11-30 00:14:03 -0500

    Pins added

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,14 +6,74 @@ function initMap() {
       lng: -122.419416
     };
 
+    var austin = {
+      lat: 30.267153,
+      lng: -97.743061
+    };
+
     var map = new google.maps.Map(
       document.getElementById('map'), {
         zoom: 4,
-        center: sf
-      });
+        center: austin
+    });
 
     var marker = new google.maps.Marker({
       position: sf,
+      map: map
+    });
+
+    var la = {
+      lat: 34.052234,
+      lng: -118.243685
+    };
+
+    var marker2 = new google.maps.Marker({
+      position: la,
+      map: map
+    });
+
+    var sj = {
+      lat: 37.338208,
+      lng: -121.886329
+    };
+
+    var marker3 = new google.maps.Marker({
+      position: sj,
+      map: map
+    });
+
+    var cupertino = {
+      lat: 37.322998,
+      lng: -122.032182
+    };
+
+    var marker4 = new google.maps.Marker({
+      position: cupertino,
+      map: map
+    });
+
+    var marker5 = new google.maps.Marker({
+      position: austin,
+      map: map
+    });
+
+    var westlala = {
+      lat: 40.425869,
+      lng: -86.908066
+    };
+
+    var marker5 = new google.maps.Marker({
+      position: westlala,
+      map: map
+    });
+
+    var ny = {
+      lat: 40.712775,
+      lng: -74.005
+    };
+
+    var marker5 = new google.maps.Marker({
+      position: ny,
       map: map
     });
 
