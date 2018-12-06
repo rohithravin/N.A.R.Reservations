19e5c1d87cdf8d42628c738e1091097118c836ef
Rohith Ravindranath <rohithravin@gmail.com>
2018-12-05 19:26:06 -0500

    minor udpate

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var mongoose=require('mongoose')
 var bodyParser=require('body-parser')
 var app = express();
 
-var server = app.listen(3000, () => {
+var server = app.listen(process.env.PORT || 3000, () => {
  console.log('server is running on port', server.address().port);
 });
 
